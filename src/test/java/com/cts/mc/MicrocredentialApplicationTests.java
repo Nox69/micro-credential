e612c176cc3fae197befcbe40be437af41ece7bd
Bharat <bharatkumar.c@cognizant.com>
2020-06-17 21:31:38 +0530

    First commit

---
+++ src/test/java/com/cts/mc/MicrocredentialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocredentialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
